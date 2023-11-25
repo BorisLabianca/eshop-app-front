a2d03d5e5c4df29eed11199ea242a47e0cd1057c
Boris Labianca <boris.labianca@hotmail.fr>
2023-11-25 14:31:06 +0800

    designed Login page

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,5 +1,38 @@
+import loginImg from "../../assets/login.png";
+import { Link } from "react-router-dom";
+import styles from "./auth.module.scss";
+import { FaGoogle } from "react-icons/fa";
+import Card from "../../components/card/Card";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <section className={`container ${styles.auth}`}>
+      <div className={styles.img}>
+        <img src={loginImg} alt="Login" width="400px" />
+      </div>
+      <Card>
+        <div className={styles.form}>
+          <h2>Login</h2>
+          <form>
+            <input type="email" placeholder="Email" required />
+            <input type="password" placeholder="Password" required />
+            <button className="--btn --btn-primary --btn-block">Login</button>
+            <div className={styles.links}>
+              <Link to="/reset-password">Forgot Password?</Link>
+            </div>
+            <p>-- or --</p>
+          </form>
+          <button className="--btn --btn-danger --btn-block">
+            <FaGoogle color="#fff" /> Login With Google
+          </button>
+          <span className={styles.register}>
+            <p>Don't have an account?</p>
+            <Link to="/register">Register</Link>
+          </span>
+        </div>
+      </Card>
+    </section>
+  );
 };
 
 export default Login;

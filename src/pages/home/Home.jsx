464c39f17f1256e96707dc4bd57681e6a525cd50
Boris Labianca <boris.labianca@hotmail.fr>
2023-12-07 21:42:37 +0800

    created basic structure of home page and subcomponents

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,12 @@
-import AdminOnlyRoute from "../../components/adminOnlyRoute/AdminOnlyRoute";
+import Product from "../../components/product/Product";
 import Slider from "../../components/slider/Slider";
 import styles from "./Home.module.scss";
 
 const Home = () => {
   return (
     <div>
-      <Slider />
-      <AdminOnlyRoute />
+      {/* <Slider /> */}
+      <Product />
     </div>
   );
 };

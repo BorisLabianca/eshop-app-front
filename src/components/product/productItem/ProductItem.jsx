b470f5d2ee2cdad5eb64c91f8811bb59c8eef7aa
Boris Labianca <boris.labianca@hotmail.fr>
2023-12-08 19:29:27 +0800

    created function to shorten text and added content to productItem

--- src/components/product/productItem/ProductItem.jsx
+++ src/components/product/productItem/ProductItem.jsx
@@ -1,7 +1,43 @@
+import { Link } from "react-router-dom";
+import Card from "../../card/Card";
 import styles from "./ProductItem.module.scss";
 
-const ProductItem = () => {
-  return <div>ProductItem</div>;
+const ProductItem = ({
+  grid,
+  product,
+  id,
+  name,
+  price,
+  description,
+  imageURL,
+}) => {
+  const shortenText = (text, lngth) => {
+    if (text.length > lngth) {
+      const shortenedText = text.substring(0, lngth).concat(" ...");
+      return shortenedText;
+    }
+    return text;
+  };
+
+  return (
+    <Card cardClass={grid ? `${styles.grid}` : `${styles.list}`}>
+      <Link to={`/product-details`}>
+        <div className={styles.img}>
+          <img src={imageURL} alt={name} />
+        </div>
+      </Link>
+      <div className={styles.content}>
+        <div className={styles.details}>
+          <p>${`${price}`}</p>
+          <h4>{shortenText(name, 18)}</h4>
+        </div>
+        {!grid && (
+          <p className={styles.description}>{shortenText(description, 200)}</p>
+        )}
+        <button className="--btn --btn-danger">Add to Cart</button>
+      </div>
+    </Card>
+  );
 };
 
 export default ProductItem;

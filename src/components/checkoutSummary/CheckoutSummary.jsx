17136c8bd5ab7a067de86f5da3556ed95e81e4f5
Boris Labianca <boris.labianca@hotmail.fr>
2023-12-11 22:10:22 +0800

    added CheckoutSummary component

---
+++ src/components/checkoutSummary/CheckoutSummary.jsx
@@ -0,0 +1,52 @@
+import { useSelector } from "react-redux";
+import styles from "./CheckoutSummary.module.scss";
+import { Link } from "react-router-dom";
+import Card from "../card/Card";
+
+const CheckoutSummary = () => {
+  const { cartItems, cartTotalAmount, cartTotalQuantity } = useSelector(
+    (store) => store.cart
+  );
+  console.log(cartItems);
+  console.log(cartTotalAmount);
+  console.log(cartTotalQuantity);
+
+  return (
+    <div>
+      <h3>Checkout Summary</h3>
+      {cartItems.length === 0 ? (
+        <>
+          <p>There are no items in your cart.</p>
+          <button className="--btn">
+            <Link to="/#products">Back To Shop</Link>
+          </button>
+        </>
+      ) : (
+        <div>
+          <p>
+            <b>{`Cart item${
+              cartTotalQuantity > 1 ? "s" : ""
+            }: ${cartTotalQuantity}`}</b>
+          </p>
+          <div className={styles.text}>
+            <h4>Total</h4>
+            <h3>${cartTotalAmount.toFixed(2)}</h3>
+          </div>
+          {cartItems.map((item) => {
+            const { id, name, price, cartQuantity } = item;
+            return (
+              <Card cardClass={styles.card} key={id}>
+                <h4>Product: {name}</h4>
+                <p>Quantity: {cartQuantity}</p>
+                <p>Unit Price: ${price.toFixed(2)}</p>
+                <p>Subtotal: ${(price * cartQuantity).toFixed(2)}</p>
+              </Card>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CheckoutSummary;

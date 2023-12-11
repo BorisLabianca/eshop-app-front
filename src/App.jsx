47bf8338a9d707f7c867d7ad1450f480c58d98f5
Boris Labianca <boris.labianca@hotmail.fr>
2023-12-11 18:33:19 +0800

    created CheckoutDetails component

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ import "./App.css";
 import AdminOnlyRoute from "./components/adminOnlyRoute/AdminOnlyRoute";
 import FallBackPage from "./pages/FallBackPage";
 import ProductDetails from "./components/product/productDetails/ProductDetails";
+import CheckoutDetails from "./pages/checkout/CheckoutDetails";
 function App() {
   return (
     <>
@@ -41,6 +42,7 @@ function App() {
           />
           <Route path="/product-details/:id" element={<ProductDetails />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout-details" element={<CheckoutDetails />} />
           <Route path="*" element={<FallBackPage />} />
         </Routes>
         <Footer
---
+++ src/pages/checkout/CheckoutDetails.jsx
@@ -0,0 +1,215 @@
+import { useState } from "react";
+import styles from "./CheckoutDetails.module.scss";
+import Card from "../../components/card/Card";
+import { CountryDropdown } from "react-country-region-selector";
+
+const initialAddressState = {
+  name: "",
+  line1: "",
+  line2: "",
+  city: "",
+  state: "",
+  postal_code: "",
+  country: "",
+  phone: "",
+};
+
+const CheckoutDetails = () => {
+  const [shippingAddress, setShippingAddress] = useState({
+    ...initialAddressState,
+  });
+  const [billingAddress, setBillingAddress] = useState({
+    ...initialAddressState,
+  });
+
+  const handleShippingAddress = (event) => {
+    const { name, value } = event.target;
+    setShippingAddress({ ...shippingAddress, [name]: value });
+  };
+
+  const handleBillingAddress = (event) => {
+    const { name, value } = event.target;
+    setBillingAddress({ ...billingAddress, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Shipping Address: ", shippingAddress);
+    console.log("Billing Address: ", billingAddress);
+  };
+
+  return (
+    <section>
+      <div className={`container ${styles.checkout}`}>
+        <h2>Checkout Details</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <Card cardClass={styles.card}>
+              <h3>Shipping Address</h3>
+              <label>Recipient Name</label>
+              <input
+                type="text"
+                placeholder="Recipient name"
+                required
+                name="name"
+                value={shippingAddress.name}
+                onChange={(event) => handleShippingAddress(event)}
+              />
+              <label>Address Line 1</label>
+              <input
+                type="text"
+                placeholder="Address line 1"
+                required
+                name="line1"
+                value={shippingAddress.line1}
+                onChange={(event) => handleShippingAddress(event)}
+              />
+              <label>Address Line 2</label>
+              <input
+                type="text"
+                placeholder="Address line 2"
+                name="line2"
+                value={shippingAddress.line2}
+                onChange={(event) => handleShippingAddress(event)}
+              />
+              <label>City</label>
+              <input
+                type="text"
+                placeholder="City"
+                required
+                name="city"
+                value={shippingAddress.city}
+                onChange={(event) => handleShippingAddress(event)}
+              />
+              <label>State</label>
+              <input
+                type="text"
+                placeholder="State"
+                required
+                name="state"
+                value={shippingAddress.state}
+                onChange={(event) => handleShippingAddress(event)}
+              />
+              <label>Postal Code</label>
+              <input
+                type="text"
+                placeholder="Postal code"
+                required
+                name="postal_code"
+                value={shippingAddress.postal_code}
+                onChange={(event) => handleShippingAddress(event)}
+              />
+              <label>Country</label>
+              <CountryDropdown
+                className={styles.select}
+                valueType="short"
+                value={shippingAddress.country}
+                onChange={(val) =>
+                  handleShippingAddress({
+                    target: {
+                      name: "country",
+                      value: val,
+                    },
+                  })
+                }
+              />
+              <label>Phone</label>
+              <input
+                type="text"
+                placeholder="Phone"
+                required
+                name="phone"
+                value={shippingAddress.phone}
+                onChange={(event) => handleShippingAddress(event)}
+              />
+            </Card>
+            <Card cardClass={styles.card}>
+              <h3>Billing Address</h3>
+              <label>Recipient Name</label>
+              <input
+                type="text"
+                placeholder="Name"
+                required
+                name="name"
+                value={billingAddress.name}
+                onChange={(event) => handleBillingAddress(event)}
+              />
+              <label>Address Line 1</label>
+              <input
+                type="text"
+                placeholder="Address line 1"
+                required
+                name="line1"
+                value={billingAddress.line1}
+                onChange={(event) => handleBillingAddress(event)}
+              />
+              <label>Address Line 2</label>
+              <input
+                type="text"
+                placeholder="Address line 2"
+                name="line2"
+                value={billingAddress.line2}
+                onChange={(event) => handleBillingAddress(event)}
+              />
+              <label>City</label>
+              <input
+                type="text"
+                placeholder="City"
+                required
+                name="city"
+                value={billingAddress.city}
+                onChange={(event) => handleBillingAddress(event)}
+              />
+              <label>State</label>
+              <input
+                type="text"
+                placeholder="State"
+                required
+                name="state"
+                value={billingAddress.state}
+                onChange={(event) => handleBillingAddress(event)}
+              />
+              <label>Postal Code</label>
+              <input
+                type="text"
+                placeholder="Postal code"
+                required
+                name="postal_code"
+                value={billingAddress.postal_code}
+                onChange={(event) => handleBillingAddress(event)}
+              />
+              <label>Country</label>
+              <CountryDropdown
+                className={styles.select}
+                valueType="short"
+                value={billingAddress.country}
+                onChange={(val) =>
+                  handleBillingAddress({
+                    target: {
+                      name: "country",
+                      value: val,
+                    },
+                  })
+                }
+              />
+              <label>Phone</label>
+              <input
+                type="text"
+                placeholder="Phone"
+                required
+                name="phone"
+                value={billingAddress.phone}
+                onChange={(event) => handleBillingAddress(event)}
+              />
+              <button type="submit" className="--btn --btn-primary">
+                Proceed To Checkout
+              </button>
+            </Card>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default CheckoutDetails;

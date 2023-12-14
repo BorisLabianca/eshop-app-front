23ef3d5bf795a9f305c08beb484f2f1498200f2a
Boris Labianca <boris.labianca@hotmail.fr>
2023-12-14 22:53:19 +0800

    completed CheckoutSuccess component

--- src/pages/checkout/CheckoutSuccess.jsx
+++ src/pages/checkout/CheckoutSuccess.jsx
@@ -1,8 +1,17 @@
+import { Link } from "react-router-dom";
+
 const CheckoutSuccess = () => {
   return (
-    <div>
-      <h1>CheckoutSuccess</h1>
-    </div>
+    <section>
+      <div className="container">
+        <h2>Checkout Successful</h2>
+        <p>Thank you for your purchase.</p>
+        <br />
+        <Link to="/order-history">
+          <button className="--btn --btn-primary">View Order Status</button>
+        </Link>
+      </div>
+    </section>
   );
 };
 
